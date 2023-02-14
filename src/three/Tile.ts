2e34fbf5fa7f3266074e08c9210287a467df3b36
Bryce Colson <bryzcolson@gmail.com>
2023-02-14 09:50:01 -0500

    Added linting

--- src/three/Tile.ts
+++ src/three/Tile.ts
@@ -1,5 +1,6 @@
 import { Hexagon } from './Hexagon';
-import { Noise, NoiseFactory } from './Noise';
+import { Noise } from './Noise';
+import { NoiseFactory } from './NoiseFactory';
 
 export enum TileType {
     SHEEP,
@@ -8,48 +9,49 @@ export enum TileType {
     BRICK,
     WOOD,
     WATER,
-    DESERT
+    DESERT,
 }
 
 export class Tile {
-  outerHexagon:Hexagon;
+    outerHexagon: Hexagon;
 
-  innerHexagon:Hexagon;
+    innerHexagon: Hexagon;
 
-  noiseMap:Noise;
+    noiseMap: Noise;
 
-  tileType:TileType;
+    tileType: TileType;
 
-  constructor(x:number, y:number, innerRadius:number, outerRadius:number, tileType: TileType) {
-    // Initialize variables
-    this.innerHexagon = new Hexagon(x, y, innerRadius);
-    this.outerHexagon = new Hexagon(x, y, outerRadius);
-    this.tileType = tileType;
-    this.noiseMap = NoiseFactory.noiseFromTileType(this.tileType, this.innerHexagon);
-  }
+    constructor(x: number, y: number, innerRadius: number, outerRadius: number, tileType: TileType) {
+        // Initialize variables
+        this.innerHexagon = new Hexagon(x, y, innerRadius);
+        this.outerHexagon = new Hexagon(x, y, outerRadius);
+        this.tileType = tileType;
+        this.noiseMap = NoiseFactory.noiseFromTileType(this.tileType, this.innerHexagon);
+    }
 
-  /**
+    /**
      * Computes if a world coordinate lies within the boundaries of this Tile
      * @param x world x coordinate
      * @param y world y coordinate
      */
-  doesOwnPoint(x:number, y:number):boolean {
-    return this.outerHexagon.distanceToHexagon(x, y) > 0;
-  }
-
-  getHeight(x:number, y:number):number {
-    return this.noiseMap.getPerlin(x, y);
-  }
-
-  getColor(): number[] {
-    if (this.tileType === TileType.SHEEP) {
-      return [121, 208 + Math.random() * 15, 33 + Math.random() * 15];
+    doesOwnPoint(x: number, y: number): boolean {
+        return this.outerHexagon.distanceToHexagon(x, y) > 0;
     }
-    if (this.tileType === TileType.STONE) {
-      return [181, 191 + Math.random() * 15, 190 + Math.random() * 15];
-    } if (this.tileType === TileType.WHEAT) {
-      return [251, 221 + Math.random() * 15, 126 + Math.random() * 15];
+
+    getHeight(x: number, y: number): number {
+        return this.noiseMap.getPerlin(x, y);
     }
-    return [0, 0, 0];
-  }
+
+    getColor(): number[] {
+        if (this.tileType === TileType.SHEEP) {
+            return [121, 208 + Math.random() * 15, 33 + Math.random() * 15];
+        }
+        if (this.tileType === TileType.STONE) {
+            return [181, 191 + Math.random() * 15, 190 + Math.random() * 15];
+        }
+        if (this.tileType === TileType.WHEAT) {
+            return [251, 221 + Math.random() * 15, 126 + Math.random() * 15];
+        }
+        return [0, 0, 0];
+    }
 }
